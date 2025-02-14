2493bd149b6c160445c60fd061f479351cd88fbb
voidkuma <168865116+voidkuma@users.noreply.github.com>
2025-02-14 10:29:43 -0800

    rnadom changes #3

--- proj2/src/DSVWriter.cpp
+++ proj2/src/DSVWriter.cpp
@@ -3,12 +3,25 @@
 #include <iostream>
 #include "DSVWriter.h"
 
+struct CDSVWriter::SImplementation{
+    std::unique_ptr<SImplementation> DImplementation; //Initalizing unique_ptr DImplementation
+    std::shared_ptr<CDataSink> Sink; //Initalizing shared_ptr Sink
+    char Delimiter; //Initalizing delimiting Character variable
+    bool QuoteAll; //Initalizing bool quoteall variable
+
+    SImplementation(std::shared_ptr<CDataSink> sink, char delimiter, bool quoteall) {
+        Sink = std::move(sink);
+        Delimiter = delimiter;
+        QuoteAll = quoteall;
+    }
+};
+
 // From Assignment: 
 // Constructor for DSV writer, sink specifies the data destination, delimiter
 // specifies the delimiting character, and quoteall specifies if all values
 // should be quoted or only those that contain the delimiter, a double quote,
 // or a newline
-CDSVWriter::(CDSVWriter(std::shared_ptr< CDataSink > sink, char delimiter, bool quoteall = false)){
+CDSVWriter::CDSVWriter(std::shared_ptr< CDataSink > sink, char delimiter, bool quoteall = false){
     std::cout << "CDSVWriter constructor" << std::endl;
 }
 
